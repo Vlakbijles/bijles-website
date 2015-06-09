0be5ca46fc393123b63a52dbf45a6b5416d8a147
nycrea <robinklusman@gmail.com>
2015-06-09 17:16:50 +0200

    working

--- dev/profiel.php
+++ dev/profiel.php
@@ -2,9 +2,8 @@
 <html>
 
     <!-- Retrieve data from API first -->
-
     <?php
-        // -- Retrieve data from API
+        // Retrieve data from API
         // TODO Authentication
         // TODO http get
 
@@ -14,7 +13,7 @@
 
         $request_uri = "/user/user_id";
         $request_method = "GET";
-        $private_key = 32487f987a897987d8739d8a987ff987;
+        $private_key = "32487f987a897987d8739d8a987ff987";
 
         $user_data_json = "";
 
@@ -22,19 +21,24 @@
     ?>
 
     <head>
-        <title>Vlakbijles.nl - <?php echo $page_title; ?><title>
+        <title>
+            Vlakbijles.nl - <?php echo $page_title; ?>
+        </title>
+        <meta name = "viewport" content = "width = device-width, initial-scale = 1.0">
+        <link href = "css/bootstrap.min.css" rel = "stylesheet">
+        <link href = "css/style.css" rel = "stylesheet">
     </head>
 
     <body>
-    <!-- Navbar at top of page -->
-    <?php include "navbar.php"; ?>
+        <!-- Navbar at top of page -->
+        <?php include "navbar.php"; ?>
 
-    <!-- content -->
 
-    <!-- Scripts -->
-    <script src = "http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-    <script src = "js/bootstrap.js"></script>
-    <script src = "js/costum.js"></script>
+
+        <!-- content -->
+
+        <!-- Scripts -->
+        <script src = "js/bootstrap.min.js"></script>
     </body>
 
 </html>
