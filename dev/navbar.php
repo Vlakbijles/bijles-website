10c1b7ac66b5473cdb4d6489088a6007f0035b65
nycrea <robinklusman@gmail.com>
2015-06-09 16:36:16 +0200

    no load fix

--- dev/navbar.php
+++ dev/navbar.php
@@ -1,16 +1,16 @@
 <!DOCTYPE html>
 <html>
-	<div class = "navbar navebar-fixed-top">
-		<div class = "navbar-inner">
-			<div class="navbar-header">
-				<a href = "index.html"  class="navbar-brand" style="color:white"><b>LOGO</b></a>
-				<a href = "index.html"  class="navbar-brand" style="color:white"><b>Vlakbijles</b></a>
-			</div>
-			<div class = "navbar-right">
-				<a href = "#" class = "navbar-brand" style="color:white" data-toggle="modal" data-target="#myModal">Login</a>
-				<a href = "#" class = "navbar-brand" style="color:white" data-toggle="modal" data-target="#myModalReg">Registreer</a>
-				<a class = "navbar-brand"> </a>
-			</div>
-		</div>
-	</div>
+    <div class = "navbar navebar-fixed-top">
+        <div class = "navbar-inner">
+            <div class="navbar-header">
+                <a href = "index.html"  class="navbar-brand" style="color:white"><b>LOGO</b></a>
+                <a href = "index.html"  class="navbar-brand" style="color:white"><b>Vlakbijles</b></a>
+            </div>
+            <div class = "navbar-right">
+                <a href = "#" class = "navbar-brand" style="color:white" data-toggle="modal" data-target="#myModal">Login</a>
+                <a href = "#" class = "navbar-brand" style="color:white" data-toggle="modal" data-target="#myModalReg">Registreer</a>
+                <a class = "navbar-brand"> </a>
+            </div>
+        </div>
+    </div>
 <html>
