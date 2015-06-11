10d1c2570f4d0696b098d1aa8feae9d59180496f
nycrea <robinklusman@gmail.com>
2015-06-11 12:06:33 +0200

    request json final

--- dev/profiel.php
+++ dev/profiel.php
@@ -19,7 +19,6 @@
 
         // Hash json
         $hash_algorithm = "sha256";
-
         $api_key = "3aced6d2d652a5a7426daabff22e372c";
         $request_uri = "/user/user_id";
         $request_method = "GET";
@@ -31,21 +30,25 @@
         hash_update($request_hash, $request_method);
         $final_hash = hash_final($request_hash);
 
-        // $request_url = "localhost{$request_uri}:5000";
-        // $request = json_encode(
-        //
-        // // Decode json & fill variables
+        $json_data["hash"] = $final_hash;
+
+        $final_request = json_encode($json_data);
+
+        // Http GET request
+        $request_url = "localhost{$request_uri}:5000";
+
+
+
+
         // $user_data = json_decode($request_response, true);
-        //
         // $user_name = $user_data["user"];
         // $user_surname = $user_data["surname"];
         // $user_picture = $user_data["picture"];
         // $user_description = $user_data["description"];
         // $user_phone = $user_data["phone"];
         // $user_email = $user_data["email"];
-        //
         // $page_title = "Profiel van {$user_name} {$user_surname}";
-        $page_title = "Profiel van";
+        $page_title = "asda";
     ?>
     <head>
         <title>
@@ -55,7 +58,7 @@
 
     <body>
         <?php
-            echo $final_hash;
+            echo $final_request;
 
             // echo $user_name;
             // echo $user_surname;
