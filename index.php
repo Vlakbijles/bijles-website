c6799b1b9e12eaac4296156b29be4878de621d0a
BDWN <bbx1992@gmail.com>
2015-06-15 20:28:17 +0200

    user id

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ require_once("api.php");
 
 // Login check
 $logged_in = false;
+$user_id = NULL;
 if(isset($_COOKIE["user_id"]) && isset($_COOKIE["token_hash"])) {
     $data = array("loggedin" => array("token_hash" => $_COOKIE["token_hash"],
                                       "user_id" => $_COOKIE["user_id"]));
@@ -12,6 +13,7 @@ if(isset($_COOKIE["user_id"]) && isset($_COOKIE["token_hash"])) {
     $response = api_request($request_uri, $request_method, $data);
     if ($response["response_code"] == 200) {
         $logged_in = true;
+        $user_id = $_COOKIE["user_id"];
     } elseif ($response["response_code"] == 0) {
         echo "api server down";
     }
