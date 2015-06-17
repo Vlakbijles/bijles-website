e544882de726b81fb5408e2a84908e2c660a2b50
BDWN <bbx1992@gmail.com>
2015-06-17 14:50:15 +0200

    refactoring, display error messages

--- offers.php
+++ offers.php
@@ -1,5 +1,7 @@
 <?php
 
+if (!isset($_GET["subject_id"]) || !isset($_GET["postal_code"])) die("Invalid URL, no subject id and/or postal code provided");
+
 require_once("api.php");
 
 $request_uri = "/offer?subject="
@@ -12,7 +14,8 @@ $resp_offers = api_request($request_uri, $request_method, NULL);
 // Render header
 switch($resp_offers["response_code"]) {
     case 200:
-        $title = "Henkies profile - Vlakbijles";
+        //TODO city and subject name in title
+        $title = "placeholder - Vlakbijles";
         break;
     default:
         $title = "Er is iets misgegaan - Vlakbijles";
@@ -32,60 +35,36 @@ echo render_template("templates/navbar.html", array(
 // Render top search bar
 echo render_template("templates/search_small.html");
 
+// Render found offers or display errors
+switch($resp_offers["response_code"]) {
 
+    case 400:
+        echo render_template("templates/error.html", array(
+                             "title" => "Er is iets misgegaan (" . $resp_offers["response_code"] . ")",
+                             "message" => "Onjuist vak en/of niet bestaande postcode ingevoerd"));
+        break;
 
-if (isset($_GET["subject_id"]) && isset($_GET["postal_code"])
-    && !empty($_GET["subject_id"]) && !empty($_GET["postal_code"])) {
+    case 404:
+        echo render_template("templates/error.html", array(
+                             "title" => "Geen resultaten (" . $resp_offers["response_code"] . ")",
+                             "message" => "Er zijn geen bijles aanbiedingen gevonden die voldoen aan de gespecifeerde criteria"));
+        break;
 
-    $request_uri = "/offer?subject="
-                    . $_GET["subject_id"]
-                    . "&loc=" . strtoupper($_GET["postal_code"])
-                    . "&page=1&range=10000000&level=2&sortby=apj";
-    $request_method = "GET";
-    $response = api_request($request_uri, $request_method, NULL);
+    case 200:
+        echo render_template("templates/offers.html", array(
+                             "results" => $resp_offers["response"]));
 
+        break;
 
-    if ($response["response_code"] == 0) {
-        echo "api server down";
-    } elseif ($response["response_code"] == 404) {
-        echo $response["response"]["message"];
-    } elseif ($response["response_code"] == 200) {
-        $search_results = $response["response"];
-    }
-    print_r($response);
-
-    if (isset($search_results)) {
-
-        $title = "Vind bijles bij jou in de buurt - Vlakbijles";
-
-        echo render_template("templates/head.html",
-                             array("title" => $title));
-
-        echo render_template("templates/navbar.html",
-                             array("logged_in" => $logged_in,
-                                   "user_id" => $user_id,
-                                   "subject_id" => $_GET["subject_id"],
-                                   "postal_code" => $_GET["postal_code"]));
-
-        include("templates/modals/register.html");
-        include("templates/modals/login.html");
-
-        echo render_template("templates/search_small.html",
-                             array("subject_id" => $_GET["subject_id"],
-                                   "postal_code" => $_GET["postal_code"]));
-
-        echo render_template("templates/offers.html",
-                             array("results" => $search_results));
-
-        include("templates/footer.html");
-
-    }
-
-
-} else {
-
-    echo "ja zo werrekt het niet";
+    default:
+        echo render_template("templates/error.html", array(
+                             "title" => "Er is iets misgegaan (-)",
+                             "message" => "Undefined error"));
+        break;
 
 }
+
+include("templates/footer.html");
+
 
 ?>
