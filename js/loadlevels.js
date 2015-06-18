001e2e9b01987770218b8b2a69f1fa92781f2f86
BDWN <bbx1992@gmail.com>
2015-06-18 16:08:27 +0200

    minor js restructuring, add subjects js

---
+++ js/loadlevels.js
@@ -0,0 +1,22 @@
+var levels = [];
+
+$(function(){
+
+    levels = (function () {
+        var formatted = [];
+        $.ajax({
+            async: true,
+            url: "ajaxutils/level.php?action=all",
+            dataType: "json",
+            success: function (data) {
+                if(data) {
+                    $.each(data, function(index, value) {
+                        $(".levelSelector").append("<option value=" + value.id + ">" + value.name + "</option>");
+                    });
+                    return data;
+                }
+            }
+        });
+    })();
+
+});
