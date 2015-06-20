8808d010fa33b1b5d46b75aa0b6a81a7a57acb54
BDWN <bbx1992@gmail.com>
2015-06-20 12:16:18 +0200

    consistency

--- vars.php
+++ vars.php
@@ -4,12 +4,14 @@ define("SITENAME", "Vlakbijles");
 define("SITETITLE", "Vind bijles bij jou in de buurt");
 
 // Error messages
-define("INVALIDSEARCH_HEADING", "Onjuiste zoekopdracht");
-define("NORESULTS_HEADING", "Geen resultaten gevonden");
-define("ERROR_HEADING", "Er is iets misgegaan");
-define("ERROR_USERNOTFOUND", "Gebruiker niet gevonden");
-define("ERROR_INVALIDSEARCH", "Onjuist vak en/of niet bestaande postcode ingevoerd");
+define("ERROR_HEADING_GENERAL", "Er is iets misgegaan");
+
+define("ERROR_HEADING_NORESULTS", "Geen resultaten gevonden");
 define("ERROR_NORESULTS", "Er zijn geen bijles aanbiedingen gevonden die voldoen aan de gespecifeerde criteria");
-define("ERROR_UNDEFINED", "Error undefined");
+
+define("ERROR_HEADING_INVALIDSEARCH", "Onjuiste zoekopdracht");
+define("ERROR_INVALIDSEARCH", "Onjuist vak en/of niet bestaande postcode ingevoerd");
+
+define("ERROR_USERNOTFOUND", "Gebruiker niet gevonden");
 
 ?>
