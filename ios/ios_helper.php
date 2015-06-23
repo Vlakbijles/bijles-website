6f9525b9407f689c2f804c5c9b8027d701333d04
nycrea <robinklusman@gmail.com>
2015-06-23 11:55:30 +0200

    fix include

--- ios/ios_helper.php
+++ ios/ios_helper.php
@@ -1,5 +1,5 @@
 <?php
-    require_once(api.php);
+    require_once("../api.php");
 
     $request_uri= $_SERVER["REQUEST_URI"];
     echo "URI=" . $request_uri;
@@ -7,6 +7,7 @@
     echo "METHOD=" . $request_method;
     $data = $HTTP_RAW_POST_DATA;
     echo "DATA=" . $data;
+    $data = json_decode($data, true);
 
     $response_array = api_request($request_uri, $request_method, $data);
     $response_json = json_encode($response_array);
