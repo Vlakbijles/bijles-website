8285073e06fc7cd1b75015e470ee1106156b9170
nycrea <robinklusman@gmail.com>
2015-06-23 12:19:16 +0200

    get test

--- ios/ios_helper.php
+++ ios/ios_helper.php
@@ -1,16 +1,14 @@
 <?php
     require_once("../api.php");
 
-    $request_uri= $_SERVER["REQUEST_URI"];
-    echo "URI=" . $request_uri;
-    $request_method = "GET"; //$_SERVER["REQUEST_METHOD"];
-    echo "METHOD=" . $request_method;
-    $data = $HTTP_RAW_POST_DATA;
-    echo "DATA=" . $data;
-    $data = json_decode($data, true);
+    // $recv = $HTTP_RAW_POST_DATA;
+    // $recv = json_decode($rcv, true);
 
-    $response_array = api_request($request_uri, $request_method, $data);
-    $response_json = json_encode($response_array);
+    $uri = "/user/1?"; //$recv["uri"];
+    $method = "GET"; //$recv["method"];
+    $data = NULL; //$recv["data"];
 
-    echo $response_json;
+
+    $resp = (api_request($uri, $method, $data));
+    echo json_encode(resp);
 ?>
