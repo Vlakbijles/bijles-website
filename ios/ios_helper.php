a29548f89f4b1ffe54f6509fe74c20d9f884d3dc
nycrea <robinklusman@gmail.com>
2015-06-23 12:24:56 +0200

    variables recv

--- ios/ios_helper.php
+++ ios/ios_helper.php
@@ -1,12 +1,12 @@
 <?php
     require_once("../api.php");
 
-    // $recv = $HTTP_RAW_POST_DATA;
-    // $recv = json_decode($rcv, true);
+    $recv = $HTTP_RAW_POST_DATA;
+    $recv = json_decode($rcv, true);
 
-    $uri = "/user/1?"; //$recv["uri"];
-    $method = "GET"; //$recv["method"];
-    $data = NULL; //$recv["data"];
+    $uri = $recv["uri"];
+    $method = $recv["method"];
+    $data = $recv["data"];
 
 
     $resp = (api_request($uri, $method, $data));
