4b699518d25aec7a25ec63c14a65b18978764de0
BDWN <bbx1992@gmail.com>
2015-06-25 11:13:55 +0200

    spookverhalen

--- js/fbconnect.js
+++ js/fbconnect.js
@@ -9,15 +9,15 @@
 
 // This is called with the results from from FB.getLoginStatus().
 function statusChangeCallback(response) {
-    console.log("statusChangeCallback");
+    console.log('statusChangeCallback');
     console.log(response);
-    if (response.status === "connected") {
+    if (response.status === 'connected') {
         // Logged into your app and Facebook.
         getFacebookData(response);
-    } else if (response.status === "not_authorized") {
+    } else if (response.status === 'not_authorized') {
         // The person is logged into Facebook, but not your app.
     } else {
-        // The person is not logged into Facebook, so we"re not sure if
+        // The person is not logged into Facebook, so we're not sure if
         // they are logged into this app or not.
     }
 }
@@ -33,10 +33,10 @@ function checkLoginState() {
 
 function getFacebookData(response) {
     $.ajax({
-        url: "/ajax/fb_auth.php",
-        type: "POST",
-        dataType: "json",
-        data: {"access_token": response.authResponse.accessToken}})
+        url: '/ajax/fb_auth.php',
+        type: 'POST',
+        dataType: 'json',
+        data: {'access_token': response.authResponse.accessToken}})
         .done(function(data, status, xhr) {
             switch (xhr.status) {
                 case 200:
@@ -45,7 +45,7 @@ function getFacebookData(response) {
                     location.reload();
                     break;
                 case 202:
-                    // 202, means account doesn"t exists with this fb account,
+                    // 202, means account doesn't exists with this fb account,
                     // so prompt user to make one
                     registerForm(data);
                     break;
@@ -55,7 +55,7 @@ function getFacebookData(response) {
         })
         .fail(function( jqXHR, textStatus ) {
             // Something went wrong, log error
-            console.log("Request failed: " + textStatus);
+            console.log('Request failed: ' + textStatus);
         });
 
 }
@@ -63,11 +63,11 @@ function getFacebookData(response) {
 
 window.fbAsyncInit = function() {
     FB.init({
-        appId      : "1597503327174282",
+        appId      : '1597503327174282',
         cookie     : true,  // enable cookies to allow the server to access
         // the session
         xfbml      : true,  // parse social plugins on this page
-        version    : "v2.2" // use version 2.2
+        version    : 'v2.2' // use version 2.2
     });
 
 
@@ -83,44 +83,44 @@ window.fbAsyncInit = function() {
     var js, fjs = d.getElementsByTagName(s)[0];
     if (d.getElementById(id)) return;
     js = d.createElement(s); js.id = id;
-    js.src = "//connect.facebook.net/en_US/sdk.js";
+    js.src = '//connect.facebook.net/en_US/sdk.js';
     fjs.parentNode.insertBefore(js, fjs);
-}(document, "script", "facebook-jssdk"));
+}(document, 'script', 'facebook-jssdk'));
 
 
 function registerForm(data){
 
-    charCounter("charCounter", "regDesc", 1000);
+    charCounter('charCounter', 'regDesc', 1000);
 
-    $("#modalTitle").text("Registreren");
-    $("#loginBody").addClass("hidden");
-    $("#registerBody").removeClass("hidden");
-    $("#registerFooter").removeClass("hidden");
-    $("#regEmail").val(data.email);
-    $("#regName").text(data.name);
-    $("#regSurname").text(data.surname);
-    $("#regPicture").attr("src", data.picture);
-    $("#regAccessToken").val(data.access_token);
+    $('#modalTitle').text('Registreren');
+    $('#loginBody').addClass('hidden');
+    $('#registerBody').removeClass('hidden');
+    $('#registerFooter').removeClass('hidden');
+    $('#regEmail').val(data.email);
+    $('#regName').text(data.name);
+    $('#regSurname').text(data.surname);
+    $('#regPicture').attr('src', data.picture);
+    $('#regAccessToken').val(data.access_token);
 
     // Actual validation
-    $("#registerForm").validate({
+    $('#registerForm').validate({
 
         // Called when form is filled in properly
         submitHandler: function(form) {
 
-            var Email = $(form).find("#regEmail").val();
-            var accessToken = $(form).find("#regAccessToken").val()
-            var PostalCode = $(form).find("#regPostalCode").val();
-            var Desc = $(form).find("#regDesc").val();
+            var Email = $(form).find('#regEmail').val();
+            var accessToken = $(form).find('#regAccessToken').val()
+            var PostalCode = $(form).find('#regPostalCode').val();
+            var Desc = $(form).find('#regDesc').val();
 
             $.ajax({
-                url: "/ajax/register.php",
-                type: "POST",
-                dataType: "json",
-                data: {"email": Email,
-                       "access_token": accessToken,
-                       "postal_code": PostalCode,
-                       "description": Desc},
+                url: '/ajax/register.php',
+                type: 'POST',
+                dataType: 'json',
+                data: {'email': Email,
+                       'access_token': accessToken,
+                       'postal_code': PostalCode,
+                       'description': Desc},
                 statusCode: {
                     // 400 indicates the email address is already in use, the
                     // postal code does not exist or that the max description
@@ -128,10 +128,10 @@ function registerForm(data){
                     400:
                         function(data) {
                             console.log(data);
-                            if (data.responseJSON.message.match("Postal code")) {
-                                $("#regPostalCode").closest(".form-group").addClass("has-error");
-                            } else if (data.responseJSON.message.match("Email")) {
-                                $("#regEmail").closest(".form-group").addClass("has-error");
+                            if (data.responseJSON.message.match('Postal code')) {
+                                $('#regPostalCode').closest('.form-group').addClass('has-error');
+                            } else if (data.responseJSON.message.match('Email')) {
+                                $('#regEmail').closest('.form-group').addClass('has-error');
                             }
                         }
                 }})
@@ -141,46 +141,46 @@ function registerForm(data){
         },
 
         rules: {
-            "regEmail": {
+            'regEmail': {
                 required: true,
                 email: true,
                 remote: {
-                    url: "/ajax/verify.php",
-                    type: "GET",
+                    url: '/ajax/verify.php',
+                    type: 'GET',
                     data: {
-                        "verify_type": "email",
-                        "verify_data": function() {
-                            return $("#regEmail").val();
+                        'verify_type': 'email',
+                        'verify_data': function() {
+                            return $('#regEmail').val();
                         }
                     }
                 }
             },
-            "regAccessToken": {
+            'regAccessToken': {
                 required: true,
             },
-            "regPostalCode": {
+            'regPostalCode': {
                 required: true,
                 remote: {
-                    url: "/ajax/verify.php",
-                    type: "GET",
+                    url: '/ajax/verify.php',
+                    type: 'GET',
                     data: {
-                        "verify_type": "postal_code",
-                        "verify_data": function() {
-                            return $("#regPostalCode").val();
+                        'verify_type': 'postal_code',
+                        'verify_data': function() {
+                            return $('#regPostalCode').val();
                         }
                     }
                 }
             },
-            "regDesc": {
+            'regDesc': {
                 maxlength: 1000,
             },
         },
 
         // Empty error messages, bootstrap indicators used instead
-        messages: { "regEmail": {required: "", email: "", remote: ""},
-                    "regAccessToken": {required: ""},
-                    "regPostalCode": {required: "", postalcode: "", remote: ""},
-                    "regDesc": {maxlength: ""} }
+        messages: { 'regEmail': {required: '', email: '', remote: ''},
+                    'regAccessToken': {required: ''},
+                    'regPostalCode': {required: '', postalcode: '', remote: ''},
+                    'regDesc': {maxlength: ''} }
 
     });
 
